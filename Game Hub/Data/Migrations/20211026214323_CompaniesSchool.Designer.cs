6ed0421cc2b0eb93b4eac7a56f1e84e7b3341f23
Benny Lu <ac98777@avcol.school.nz>
2021-10-29 14:54:50 +1300

    Added Migrations for the current CRUD pages

--- Game Hub/Data/Migrations/20211026214323_CompaniesSchool.Designer.cs
+++ Game Hub/Data/Migrations/20211026214323_CompaniesSchool.Designer.cs
@@ -4,14 +4,16 @@ using Game_Hub.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Game_Hub.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211026214323_CompaniesSchool")]
+    partial class CompaniesSchool
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Game Hub/Data/Migrations/20211026214343_GamesSchool.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Game_Hub.Data.Migrations
+{
+    public partial class GamesSchool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
