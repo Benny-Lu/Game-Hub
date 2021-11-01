48ade3b41db0dc42e8d38dd281450adf95175e5e
Benny Lu <ac98777@avcol.school.nz>
2021-11-02 11:33:19 +1300

    +Added 3 Basic Queries to Find the Author, Company, and Games tables

---
+++ Queries/GetAuthor.sql
@@ -0,0 +1,10 @@
+﻿USE [aspnet-Game_Hub-70EDBC99-917B-4751-8DFD-8C25E435BA2E]
+GO
+
+DECLARE	@return_value Int
+
+EXEC	@return_value = [dbo].[SelectGames]
+
+SELECT	@return_value as 'Return Value'
+
+GO
